8f79ff6e3920d5cf3bafd1b1d69ac50d7804f9fb
danjuan-77 <youngertutu@gmail.com>
2025-04-23 01:23:38 +0800

    update run task shell

--- run_task.sh
+++ run_task.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_wenxi_lr4.sh
 
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_wenxi_lr5.sh
 
-bash examples/s2s/scripts/finetune/finetune_s2s_interleave_lora_ultravoice_sft.sh # lr 0.4
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr4.sh
 
-bash examples/s2s/scripts/finetune/finetune_s2s_interleave_lora_ultravoice_sft_lr5.sh # lr 0.5
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr5.sh
 
-bash examples/s2s/scripts/finetune/finetune_s2s_interleave_lora_ultravoice_sft_lr3.sh # lr 0.3
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_volume_lr4.sh
 
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_volume_lr5.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_speed_lr4.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_speed_lr5.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_desc_lr4.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_desc_lr5.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_accent_lr4.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_accent_lr5.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_lang_lr4.sh
+
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_lang_lr5.sh
 
 # nohup bash run_task.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/run_task_slamomni_sft_$(date +%Y%m%d%H%M%S).log 2>&1 &
