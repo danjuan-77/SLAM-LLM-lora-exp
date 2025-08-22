04dee97749e240a2094861a86c48279e0ca85c27
danjuan-77 <youngertutu@gmail.com>
2025-08-22 17:12:29 +0800

    update scripts

--- run_task_node1.sh
+++ run_task_node1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_speed_lr5.sh
+# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_speed_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_speed_lr5.sh
 
-bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_volume_lr5.sh
+# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_volume_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_volume_lr5.sh
 
-bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr5.sh
+# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_emotion_lr5.sh
 
 
 # nohup bash run_task_node1.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/run_task_slamomni_sft_node1_$(date +%Y%m%d%H%M%S).log 2>&1 &
+
+# nohup bash run_task_node1.sh > /share/nlp/kangyipeng/logs/run_task_slamomni_inference_node1_$(date +%Y%m%d%H%M%S).log 2>&1 &
--- run_task_node2.sh
+++ run_task_node2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_desc_lr5.sh
-# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_desc_lr5.sh
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_desc_lr5.sh
 
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_accent_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_accent_lr5.sh
 
-bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_lang_lr5.sh
+# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_lang_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_lang_lr5.sh
 
 # nohup bash run_task_node2.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/run_task_slamomni_sft_node2_$(date +%Y%m%d%H%M%S).log 2>&1 &
+
+# nohup bash run_task_node2.sh > /share/nlp/kangyipeng/logs/run_task_slamomni_inference_node2_$(date +%Y%m%d%H%M%S).log 2>&1 &
