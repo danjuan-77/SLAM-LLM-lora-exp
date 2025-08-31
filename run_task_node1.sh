86b995185ed2ca34cd11235c98857566512a44a1
danjuan-77 <youngertutu@gmail.com>
2025-08-31 20:23:39 +0800

    update scripts

--- run_task_node1.sh
+++ run_task_node1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_speed_lr5.sh
-# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_speed_lr5.sh
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_speed_lr5.sh
 
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_volume_lr5.sh
-# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_volume_lr5.sh
+bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_volume_lr5.sh
 
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr5.sh
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_emotion_lr5.sh
