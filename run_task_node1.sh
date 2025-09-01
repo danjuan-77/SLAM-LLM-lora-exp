f5b56a4debc5b94b33c656b732dab29a2cbd46e5
danjuan-77 <youngertutu@gmail.com>
2025-09-01 09:39:44 +0800

    update

--- run_task_node1.sh
+++ run_task_node1.sh
@@ -6,7 +6,7 @@ bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_s
 bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_volume_lr5.sh
 
 # bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_gqa_emotion_lr5.sh
-bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_emotion_lr5.sh
+# bash examples/s2s/scripts/finetune/slam-omni0.5b/finetune_s2s_group_ultravoice_sft_emotion_lr5.sh
 
 
 # nohup bash run_task_node1.sh > /share/nlp/tuwenming/projects/UltraVoice_dev/logs/run_task_slamomni_sft_node1_$(date +%Y%m%d%H%M%S).log 2>&1 &
